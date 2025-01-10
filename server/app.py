c99e4889974b5eff25f6b6b2be16d3addbc20518
Snow_S <2728749584@qq.com>
2025-01-10 10:28:14 +0800

    添加静态文件接口

--- server/app.py
+++ server/app.py
@@ -6,12 +6,15 @@ from starlette.middleware.cors import CORSMiddleware
 from server.init import initiate_database
 from api.v1.router import router as api_v1_router
 from middleware.response import CommonResponse
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI(
     title="Celeste Talk API",
     description="FastAPI based chat application",
     version="1.0.0"
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 # API版本路由
 app.include_router(api_v1_router, prefix="/api/v1")
 app.add_middleware(
